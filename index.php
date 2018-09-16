0b0e118d9940cb6638418748306a5a89b1031b4d
David Callizaya <davidcallizaya@gmail.com>
2018-09-15 23:29:26 -0400

    avance

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
-<?php
+<html>
+  <head>
+    <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.css" media="screen">
+  </head>
+  <body>
+    <script src="https://bootswatch.com/_vendor/jquery/dist/jquery.min.js"></script>
+    <script src="https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
 
-require 'config.php';
-
-var_dump(search('test'));
